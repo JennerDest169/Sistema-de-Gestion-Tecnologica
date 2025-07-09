bf68f48f41ed75d5fa4607f5783ec8a88f6c7633
JennerDest169 <cabelloalvaradojenner@gmail.com>
2025-07-09 00:15:02 -0400

    Proyecto Base

---
+++ src/main/java/com/grupo04/GestionDeEquiposTEC/GestionDeEquiposTecApplication.java
@@ -0,0 +1,13 @@
+package com.grupo04.GestionDeEquiposTEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeEquiposTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeEquiposTecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo04/GestionDeEquiposTEC/GestionDeEquiposTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo04.GestionDeEquiposTEC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeEquiposTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
