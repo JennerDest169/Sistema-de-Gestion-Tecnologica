42fec2716dfbe3a09add2142a5318bca5a4bd163
JennerDest <cabelloalvaradojenner@gmail.com>
2025-07-11 19:01:58 -0500

    extras

--- src/main/java/com/grupo04/GestionDeEquiposTEC/Repository/PrestamosRepository.java
+++ src/main/java/com/grupo04/GestionDeEquiposTEC/Repository/PrestamosRepository.java
@@ -1,2 +1,7 @@
-package com.grupo04.GestionDeEquiposTEC.Repository;public interface PrestamosRepository {
+package com.grupo04.GestionDeEquiposTEC.Repository;
+
+import com.grupo04.GestionDeEquiposTEC.Entidad.Prestamos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrestamosRepository extends JpaRepository<Prestamos,Integer> {
 }
--- src/main/java/com/grupo04/GestionDeEquiposTEC/Services/PrestamosServicio.java
+++ src/main/java/com/grupo04/GestionDeEquiposTEC/Services/PrestamosServicio.java
@@ -1,2 +1,26 @@
-package com.grupo04.GestionDeEquiposTEC.Services;public class PrestamosServicio {
+package com.grupo04.GestionDeEquiposTEC.Services;
+
+import com.grupo04.GestionDeEquiposTEC.Entidad.Prestamos;
+import com.grupo04.GestionDeEquiposTEC.Repository.PrestamosRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PrestamosServicio {
+    private final PrestamosRepository presta;
+
+    public List<Prestamos> allPrestamos(){
+        return presta.findAll();
+    }
+
+    public void crearPrestamos(Prestamos prestamos){
+        presta.save(prestamos);
+    }
+
+    public void actualizarPrestamos(Prestamos prestamos){
+        presta.save(prestamos);
+    }
 }
